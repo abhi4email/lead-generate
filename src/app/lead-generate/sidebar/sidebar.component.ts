7e292ed299eb49c9b2b1573941a757b42fef2b92
Abhishek <abhi4email@gmail.com>
2023-01-12 17:34:32 +0530

    design

--- src/app/lead-generate/sidebar/sidebar.component.ts
+++ src/app/lead-generate/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+declare var $:any;
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -10,6 +11,34 @@ export class SidebarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+ 
+      var owl = $('.owl-carousel');
+      owl.owlCarousel({
+        autoplay:true,
+        loop: true,
+        mouseDrag: false,
+        touchDrag: false,
+        pullDrag: false,
+        dots: true,
+        navSpeed: 700,
+        autoWidth:false,
+        mergeFit:false,
+        margin:40,
+        stagePadding:20,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 1
+          },
+          1000: {
+            items: 1
+          }
+        }
+      })
+   
   }
   customOptions: any = {
     autoplay:true,
@@ -22,7 +51,7 @@ export class SidebarComponent implements OnInit {
     autoWidth:false,
     mergeFit:false,
     margin:40,
-    stagePadding:30,
+    stagePadding:20,
     responsive: {
       0: {
         items: 1
